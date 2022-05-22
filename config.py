51553b511c21ce0609cd46fa13d128d4b89f8e7f
Walid Birouk <91180916+Walid-Birouk@users.noreply.github.com>
2022-05-22 22:26:39 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 HOST = "irc.twitch.tv"              # This is Twitchs IRC server
 PORT = 6667                         # Twitchs IRC server listens on port 6767
 NICK = "Secret_Agent"            # Twitch username for the bot
-PASS = "oauth:j2wwbxgwtead3f1iziomt7ws83mihp" # my Twitch OAuth token
+PASS = "oauth:**********************" # my Twitch OAuth token
 CHAN = "#espqd0n"                   # the channel you want the bot to join.
 RATE = (30) # messages per seccond
 BAN_PAT = [r"test",r"test1",r"badword",r"badword1"]
